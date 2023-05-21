2412856a4f608ec2b02124c9275f1e5583efbbd6
Oz RAGE Harm <eabot864@gmail.com>
2023-05-20 22:54:38 -0500

    updated README.md

--- examples/login-example.js
+++ examples/login-example.js
@@ -1,5 +1,4 @@
 // this is a login example using EpicCore with a console prompt for an Epic Games authentication code
-// use this link to get an auth code: https://rebrand.ly/authcode
 const { login } = require('@ozrageharm/epiccore');
 const prompt = require("prompt-sync")();
 
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 module.exports = {
 
-    platforms: require('./src/platforms.js'),
-
     login: require('./src/login.js'),
     logout: require('./src/logout.js')
 
